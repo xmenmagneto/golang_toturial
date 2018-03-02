98770194d5cd7b3df17d55a4be837cbf62d08428
Tingchang Deng <tingchangdeng@Tingchangs-MacBook-Pro.local>
2018-03-01 16:00:19 -0800

    parse xml

---
+++ 11-parse_XML/gotut.go
@@ -0,0 +1,31 @@
+package main
+
+import ("fmt"
+		"net/http"
+		"io/ioutil"
+		"encoding/xml")
+
+
+type SitemapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
+
+
+func main() {
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)  //unmarshal into memory address of s
+
+	fmt.Println(s.Locations)
+}
