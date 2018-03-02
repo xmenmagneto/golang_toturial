3f69a672e9cc7f108f76116e886a3e3cf80fcbcf
Tingchang Deng <tingchangdeng@Tingchangs-MacBook-Pro.local>
2018-03-01 20:15:35 -0800

    go routine sync

---
+++ 19-go_routine_sync/gotut.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"time"
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func say(s string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(s)
+		time.Sleep(time.Millisecond * 500)
+	}
+	wg.Done()
+}
+
+func main() {
+	wg.Add(1)
+	go say("Hey")
+	wg.Add(1)
+	go say("There")
+
+
+	wg.Add(1)
+	go say("Hi")
+	wg.Wait()
+}
